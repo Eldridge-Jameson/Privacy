6da0d28dbe2fbc67fbc7b506307013192ef2c1d1
jjpp55 <71411754+jjpp55@users.noreply.github.com>
2021-11-22 19:00:03 -0500

    Update __init__.py

--- Privacy_Initialization/__init__.py
+++ Privacy_Initialization/__init__.py
@@ -1,4 +1,3 @@
-
 from otree.api import *
 c = cu
 
@@ -8,8 +7,7 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 1
     questions = ('Are you attracted to anyone in the room?', 'Have you ever been arrested?', 'Can you imagine pursuing a political career?')
-def Update_Player_Ranking(subsession):
-    pass
+
 class Subsession(BaseSubsession):
     pass
 def displayQuestion(group):
@@ -23,52 +21,51 @@ def displayQuestion(group):
     random.shuffle(questions)
     #group.current_question = questions[0]
     
-def getTotalYes(group):
-    pass
-def calculateRewards(group):
-    pass
 class Group(BaseGroup):
     answer_yeses = models.IntegerField(initial=0)
     number_of_players = models.IntegerField(initial=0)
-def enterQuestion(player):
+
+def set_name(group):
+    for p in group.get_players():
+        participant = p.participant
+        participant.username = p.name
+
+
+def determine_host(player):
     session = player.session
-    group = player.group
-    import random
-    
-    random.shuffle(session.quesstion_bank)
-    group.current_question = session.quesstion_bank[0]
-    
-    session.quesstion_bank = ["question1","question2","question3","question 4","question 5","question 6","question 7","question8","question9"]
-    session.used_questions=[" "]
-    session.quesstion_bank.append(player.enter_question)
+    if player.host == True:
+        session.host = player.id_in_group
 def determine_valid_id(player):
     session = player.session
     group = player.group
     participant = player.participant
-    p_host= group.get_player_by_id(1)
+    p_host= group.get_player_by_id(session.host)
     session.invalid_Ids =[]
-    if(participant.id_in_session ==1):
+    if(participant.id_in_session == session.host):
         participant.is_dropout = False
     elif player.id_InGroup != p_host.host_Id:
         session.invalid_Ids.append(player.id_in_group)
         participant.is_dropout= True
     else:
         participant.is_dropout=False
+
 class Player(BasePlayer):
     id_InGroup = models.IntegerField()
-    name = models.StringField()
+    name = models.StringField(label='Enter your name')
     host = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], initial=False, label=' ')
     host_Id = models.IntegerField()
     enter_question = models.LongStringField(blank=True, label='Enter a question you would like to ask the group here:')
     is_dropout = models.BooleanField(initial=False)
+
 class Welcome_Page(Page):
     form_model = 'player'
-    form_fields = ['host']
+    form_fields = ['host','name']
     @staticmethod
     def before_next_page(player, timeout_happened):
         import random
         num = random.randint(0,189753)
         player.host_Id = num
+        determine_host(player)
 class Host_Session(Page):
     form_model = 'player'
     @staticmethod
@@ -107,6 +104,8 @@ class Invalid_ID_dropout(Page):
         return participant.is_dropout
 class WaitPage1(WaitPage):
     body_text = 'Waiting for all the players to join'
+    after_all_players_arrive = set_name
+
 class Write_Questions(Page):
     form_model = 'player'
     form_fields = ['enter_question']
